2f73086fb126d2ebbed579b5440aa97c65371c42
Lee-Jae-Ik <leeji0922@ontacthealth.com>
2025-06-21 21:40:54 +0900

    github 임시 코드 제거

--- lambda/save_files.py
+++ lambda/save_files.py
@@ -1,5 +1,6 @@
 import boto3
-from github import Github
+import requests
+from bs4 import BeautifulSoup as bs4
 
 def save_to_s3(terraform_files, s3_bucket_name, s3_bucket_key):
     s3 = boto3.client('s3')
@@ -7,7 +8,4 @@ def save_to_s3(terraform_files, s3_bucket_name, s3_bucket_key):
     return ""
 
 def push_to_github(terraform_files, github_repo_name, github_repo_branch, github_repo_token, github_repo_key):
-    github = Github(github_repo_token)
-    repo = github.get_repo(github_repo_name)
-    repo.create_file(github_repo_branch, github_repo_key, terraform_files, message="Update Terraform files")
     return ""
